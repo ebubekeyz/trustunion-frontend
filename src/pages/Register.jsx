4b2409a5f065ca005f6df8e93b5eb4809b3fabee
ebubekeyz <ebubeofforjoe@gmail.com>
2025-03-21 17:29:58 +0100

    2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -252,7 +252,7 @@ then close all select boxes: */
   }, [validate3]);
   return (
     <Wrapper>
-   <div className="hero bg-base-200 md:min-h-screen">
+   {/* <div className="hero bg-base-200 md:min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-4xl md:text-5xl font-bold text-amber-200">Open An Account!</h1>
@@ -336,10 +336,31 @@ then close all select boxes: */
       </div>
     </div>
   </div>
-</div>
+</div> */}
 
        
-
+<div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse max-w-6xl">
+    <div className="text-center lg:text-left">
+      <h1 className="text-4xl md:text-5xl font-bold text-amber-200">Register</h1>
+      <p className="py-6 text-amber-50">
+    Please ensure you enter your correct information to avoid account suspension.
+      </p>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <fieldset className="fieldset">
+          <label className="fieldset-label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="fieldset-label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div><NavLink to="/reset-password" className="link link-hover">Forgot password?</NavLink></div>
+          <button type="submit" className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+</div>
     </Wrapper>
   );
 };
