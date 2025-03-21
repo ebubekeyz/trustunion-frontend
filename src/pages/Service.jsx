8bbdaa60c00c669644169f880fb2e3398aa58032
ebubekeyz <ebubeofforjoe@gmail.com>
2025-03-21 17:06:32 +0100

    2

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -5,7 +5,7 @@ import { service } from '../utils';
 
 const Service = () => {
   return (
-    <div className="py-6 max-w-6xl mx-auto">
+    <div className="py-6 max-w-6xl mx-auto align-element">
    
          <div className='grid grid-cols-1 place-items-center md:grid md:grid-cols-3 gap-4 '>
 
