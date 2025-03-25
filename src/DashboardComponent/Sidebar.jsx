5cab124bc05006366e397b02158ab8f328e56c9d
ebubekeyz <ebubeofforjoe@gmail.com>
2025-03-25 12:33:44 +0100

    2

--- src/DashboardComponent/Sidebar.jsx
+++ src/DashboardComponent/Sidebar.jsx
@@ -221,7 +221,7 @@ const Sidebar = ({ onClick, onClick2 }) => {
         {/* start */}
         <div className="navbar bg-base-100 shadow-sm">
           <div className="flex-1">
-             <a className="text-xl">TrustunionBank</a>
+             
             <label htmlFor="my-drawer" className="drawer-button md:hidden"> <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
       </div></label>
