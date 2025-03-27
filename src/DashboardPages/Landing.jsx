ddaa1602c7cdb07c38f96f6db111b828c2e6506f
ebubekeyz <ebubeofforjoe@gmail.com>
2025-03-27 16:01:08 +0100

    2

--- src/DashboardPages/Landing.jsx
+++ src/DashboardPages/Landing.jsx
@@ -298,7 +298,7 @@ const Landing = () => {
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg> */}
  
-  <Link to="/dashboard" className="btn btn-md btn-block " >Apply for Loan</Link>
+  {/* <Link to="/dashboard" className="btn btn-md btn-block " >Apply for Loan</Link> */}
 </div>
    </>
 
